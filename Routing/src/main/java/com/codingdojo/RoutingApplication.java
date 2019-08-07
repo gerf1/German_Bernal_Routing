edaeb06bc1a59c81203463fbd7448b258cf627ce
German Bernal <ger.e.bernal@gmail.com>
2019-08-06 21:58:26 -0500

    Add files via upload

---
+++ Routing/src/main/java/com/codingdojo/RoutingApplication.java
@@ -0,0 +1,16 @@
+package com.codingdojo;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingApplication.class, args);
+		
+	}
+
+}
